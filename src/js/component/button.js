be932bc06870f710a01b033b152539b7c0c5e9be
apolopino <apolo.pino@gmail.com>
2021-06-12 17:39:53 +0000

    funciona activar y desactivar cada uno. Falta verificar y desactivar el resto si hay uno activo

--- src/js/component/button.js
+++ src/js/component/button.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+
+const stylesA = {
+	height: "70px",
+	width: "70px",
+	borderRadius: "50%",
+	display: "block",
+	boxShadow: "none"
+};
+
+const stylesB = {
+	height: "70px",
+	width: "70px",
+	borderRadius: "50%",
+	display: "block",
+	boxShadow: `0px 0px 17px 36px #FFC545`
+};
 
 export default function Button(props) {
 	return (
 		<div className="row justify-content-center pt-3 pb-3">
-			<div
+			<button
 				className={props.color}
-				style={{
-					height: "70px",
-					width: "70px",
-					borderRadius: "50%",
-					display: "block"
-				}}></div>
+				style={props.active === true ? stylesB : stylesA}></button>
 		</div>
 	);
 }
 Button.propTypes = {
-	color: PropTypes.array
+	color: PropTypes.array,
+	active: PropTypes.bool
 };
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Button from "./button.js";
 
 export function Home() {
+	const [activeRed, setActiveRed] = useState(false);
+	const [activeYellow, setActiveYellow] = useState(false);
+	const [activeGreen, setActiveGreen] = useState(false);
+
 	return (
 		<div>
 			<div className="mt-5 d-flex justify-content-center">
 				<div
 					className="card"
-					style={{ width: "150px", backgroundColor: "black" }}>
+					style={{
+						width: "150px",
+						backgroundColor: "black",
+						border: "1px solid black"
+					}}>
 					<div className="card-body">
-						<Button color="bg-danger" />
-						<Button color="bg-warning" />
-						<Button color="bg-success" />
+						<div
+							onClick={() => {
+								setActiveRed(!activeRed);
+								() =>
+									activeRed === true
+										? setActiveYellow(false)
+										: null;
+							}}>
+							<Button color="bg-danger" active={activeRed} />
+						</div>
+						<div onClick={() => setActiveYellow(!activeYellow)}>
+							<Button color="bg-warning" active={activeYellow} />
+						</div>
+						<div onClick={() => setActiveGreen(!activeGreen)}>
+							<Button color="bg-success" active={activeGreen} />
+						</div>
 					</div>
 				</div>
 			</div>
